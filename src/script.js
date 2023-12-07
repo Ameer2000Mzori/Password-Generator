1d773807ed8c8479b424abbccd65d2025e96ec2f
Ameer <ameerapex496@gmail.com>
2023-12-07 02:18:25 +0100

    selected elements + added comments

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-console.log("hallo world");
+// selected elements
+var copyBox = document.getElementsByClassName("number-Input")[0];
+var lengthOfPass = document.getElementsByClassName("length-Of-Pass")[0];
+// selected check elements
+var upperCaseCheck = document.getElementsByClassName("upperCase-Check")[0];
+var LowerCaseCheck = document.getElementsByClassName("LowerCase-Check")[0];
+var numbersCheck = document.getElementsByClassName("numbers-Check")[0];
+var symbolsCheck = document.getElementsByClassName("symbols-Check")[0];
+//gelobal variables
+// functions
+// event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log("hallo world");
+// selected elements
+const copyBox = document.getElementsByClassName("number-Input")[0];
+const lengthOfPass = document.getElementsByClassName("length-Of-Pass")[0];
+
+// selected check elements
+const upperCaseCheck = document.getElementsByClassName("upperCase-Check")[0];
+const LowerCaseCheck = document.getElementsByClassName("LowerCase-Check")[0];
+const numbersCheck = document.getElementsByClassName("numbers-Check")[0];
+const symbolsCheck = document.getElementsByClassName("symbols-Check")[0];
+
+//gelobal variables
+
+// functions
+
+// event lisnters
