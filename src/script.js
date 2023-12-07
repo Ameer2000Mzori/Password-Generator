6047bd4d4914610bd67260657acbdecc1dde2415
Ameer <ameerapex496@gmail.com>
2023-12-07 02:23:55 +0100

    added small changes to html and selected buttons

--- src/script.js
+++ src/script.js
@@ -6,6 +6,9 @@ var upperCaseCheck = document.getElementsByClassName("upperCase-Check")[0];
 var LowerCaseCheck = document.getElementsByClassName("LowerCase-Check")[0];
 var numbersCheck = document.getElementsByClassName("numbers-Check")[0];
 var symbolsCheck = document.getElementsByClassName("symbols-Check")[0];
+// seleted buttons
+var copyPassBtn = document.getElementsByClassName("copy-Pass-Btn")[0];
+var generatePassBtn = document.getElementsByClassName("generate-Pass")[0];
 //gelobal variables
 // functions
 // event lisnters
--- src/script.ts
+++ src/script.ts
@@ -8,6 +8,10 @@ const LowerCaseCheck = document.getElementsByClassName("LowerCase-Check")[0];
 const numbersCheck = document.getElementsByClassName("numbers-Check")[0];
 const symbolsCheck = document.getElementsByClassName("symbols-Check")[0];
 
+// seleted buttons
+const copyPassBtn = document.getElementsByClassName("copy-Pass-Btn")[0];
+const generatePassBtn = document.getElementsByClassName("generate-Pass")[0];
+
 //gelobal variables
 
 // functions
