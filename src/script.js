f5b0f27faa44a46f705d106ff6ed8000a34fdd6d
Ameer <ameerapex496@gmail.com>
2023-12-07 05:23:49 +0100

    cleaned up the code + added and remove cooments

--- src/script.js
+++ src/script.js
@@ -1,60 +1,15 @@
 // selected elements
 var copyBox = document.getElementsByClassName("number-Input")[0];
 var lengthOfPass = document.getElementsByClassName("length-Of-Pass")[0];
-// selected check elements
-var upperCaseCheck = document.getElementsByClassName("upperCase-Check")[0];
-var LowerCaseCheck = document.getElementsByClassName("LowerCase-Check")[0];
-var numbersCheck = document.getElementsByClassName("numbers-Check")[0];
-var symbolsCheck = document.getElementsByClassName("symbols-Check")[0];
 var checkedBoxes = document.querySelectorAll("#checked-Box");
-// seleted buttons
 var copyPassBtn = document.getElementsByClassName("copy-Pass-Btn")[0];
 var generatePassBtn = document.getElementsByClassName("generate-Pass")[0];
 //gelobal variables
 var generatedPassword;
 var lengthOfPassWord;
-var loowsArr = [];
-var pushableArry = [];
-var typesCount = 0;
-// functions
-// copy password function
-var copyPassword = function () {
-    copyBox.select();
-    copyBox.setSelectionRange(0, 99999); // For mobile devices
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyBox.value);
-    console.log(copyBox.value);
-};
-// Genorate Random Password Function
-function GenorateRandomPassword() {
-    // Initialize outside the loop for concatenation
-    generatedPassword = "";
-    for (var i = 0; i < lengthOfPassWord; i += typesCount) {
-        loowsArr.forEach(function (type) {
-            var funcName = type;
-            if (randomFunc[funcName] && typeof randomFunc[funcName] === "function") {
-                generatedPassword += randomFunc[funcName]();
-            }
-        });
-    }
-    var finalgeneratedPassword = generatedPassword.slice(0, lengthOfPassWord);
-    copyBox.value = finalgeneratedPassword;
-}
-generatePassBtn.addEventListener("click", function () {
-    lengthOfPassWord = lengthOfPass.value;
-    loowsArr = [];
-    pushableArry = [];
-    for (var _i = 0, checkedBoxes_1 = checkedBoxes; _i < checkedBoxes_1.length; _i++) {
-        var boxCheck = checkedBoxes_1[_i];
-        if (boxCheck.checked) {
-            pushableArry.push("".concat(boxCheck.value));
-        }
-    }
-    loowsArr = pushableArry;
-    typesCount = loowsArr.length;
-    GenorateRandomPassword();
-    console.log("type count length", typesCount, "our loows arry", loowsArr);
-});
+var lastArry = [];
+var tempArry = [];
+var selectedType = 0;
 // The object 'randomFunc' associates descriptive keys with corresponding functions,
 var randomFunc = {
     upperCase: generateUpperCase,
@@ -76,3 +31,45 @@ function generatesymbolsCase() {
     var symbols = "!@#$%^&*(){}[]=<>/,.";
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+// our loop logic over the selected elements function :
+var logicLoopFunction = function () {
+    lengthOfPassWord = lengthOfPass.value;
+    lastArry = [];
+    tempArry = [];
+    for (var _i = 0, checkedBoxes_1 = checkedBoxes; _i < checkedBoxes_1.length; _i++) {
+        var boxCheck = checkedBoxes_1[_i];
+        if (boxCheck.checked) {
+            tempArry.push("".concat(boxCheck.value));
+        }
+    }
+    lastArry = tempArry;
+    selectedType = lastArry.length;
+    GenorateRandomPassword();
+    console.log("type count length", selectedType, "our loows arry", lastArry);
+};
+// Genorate Random Password Function
+function GenorateRandomPassword() {
+    // Initialize outside the loop for concatenation
+    generatedPassword = "";
+    for (var i = 0; i < lengthOfPassWord; i += selectedType) {
+        lastArry.forEach(function (type) {
+            var funcName = type;
+            if (randomFunc[funcName] && typeof randomFunc[funcName] === "function") {
+                generatedPassword += randomFunc[funcName]();
+            }
+        });
+    }
+    var finalgeneratedPassword = generatedPassword.slice(0, lengthOfPassWord);
+    copyBox.value = finalgeneratedPassword;
+}
+// copy password function
+var copyPassword = function () {
+    copyBox.select();
+    copyBox.setSelectionRange(0, 99999); // For mobile devices
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyBox.value);
+    console.log(copyBox.value);
+};
+// our eventlinsters
+generatePassBtn.addEventListener("click", logicLoopFunction);
+copyPassBtn.addEventListener("click", copyPassword);
--- src/script.ts
+++ src/script.ts
@@ -1,70 +1,16 @@
 // selected elements
 let copyBox: any = document.getElementsByClassName("number-Input")[0];
 const lengthOfPass: any = document.getElementsByClassName("length-Of-Pass")[0];
-
-// selected check elements
-const upperCaseCheck: any =
-  document.getElementsByClassName("upperCase-Check")[0];
-const LowerCaseCheck: any =
-  document.getElementsByClassName("LowerCase-Check")[0];
-const numbersCheck: any = document.getElementsByClassName("numbers-Check")[0];
-const symbolsCheck: any = document.getElementsByClassName("symbols-Check")[0];
 const checkedBoxes: any = document.querySelectorAll("#checked-Box");
-
-// seleted buttons
 const copyPassBtn = document.getElementsByClassName("copy-Pass-Btn")[0];
 const generatePassBtn = document.getElementsByClassName("generate-Pass")[0];
 
 //gelobal variables
-let generatedPassword;
+let generatedPassword: string;
 let lengthOfPassWord: number;
-let loowsArr: string[] = [];
-let pushableArry: string[] = [];
-let typesCount: number = 0;
-
-// functions
-
-// copy password function
-const copyPassword = (): void => {
-  copyBox.select();
-  copyBox.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyBox.value);
-  console.log(copyBox.value);
-};
-
-// Genorate Random Password Function
-function GenorateRandomPassword() {
-  // Initialize outside the loop for concatenation
-  generatedPassword = "";
-  for (let i = 0; i < lengthOfPassWord; i += typesCount) {
-    loowsArr.forEach((type) => {
-      const funcName = type;
-      if (randomFunc[funcName] && typeof randomFunc[funcName] === "function") {
-        generatedPassword += randomFunc[funcName]();
-      }
-    });
-  }
-  let finalgeneratedPassword = generatedPassword.slice(0, lengthOfPassWord);
-  copyBox.value = finalgeneratedPassword;
-}
-
-generatePassBtn.addEventListener("click", () => {
-  lengthOfPassWord = lengthOfPass.value;
-  loowsArr = [];
-  pushableArry = [];
-  for (let boxCheck of checkedBoxes) {
-    if (boxCheck.checked) {
-      pushableArry.push(`${boxCheck.value}`);
-    }
-  }
-
-  loowsArr = pushableArry;
-  typesCount = loowsArr.length;
-  GenorateRandomPassword();
-  console.log("type count length", typesCount, "our loows arry", loowsArr);
-});
+let lastArry: string[] = [];
+let tempArry: string[] = [];
+let selectedType: number = 0;
 
 // The object 'randomFunc' associates descriptive keys with corresponding functions,
 let randomFunc = {
@@ -91,3 +37,50 @@ function generatesymbolsCase() {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+// our loop logic over the selected elements function :
+
+const logicLoopFunction = () => {
+  lengthOfPassWord = lengthOfPass.value;
+  lastArry = [];
+  tempArry = [];
+  for (let boxCheck of checkedBoxes) {
+    if (boxCheck.checked) {
+      tempArry.push(`${boxCheck.value}`);
+    }
+  }
+  lastArry = tempArry;
+  selectedType = lastArry.length;
+  GenorateRandomPassword();
+  console.log("type count length", selectedType, "our loows arry", lastArry);
+};
+
+// Genorate Random Password Function
+function GenorateRandomPassword() {
+  // Initialize outside the loop for concatenation
+  generatedPassword = "";
+  for (let i = 0; i < lengthOfPassWord; i += selectedType) {
+    lastArry.forEach((type) => {
+      const funcName = type;
+      if (randomFunc[funcName] && typeof randomFunc[funcName] === "function") {
+        generatedPassword += randomFunc[funcName]();
+      }
+    });
+  }
+  let finalgeneratedPassword = generatedPassword.slice(0, lengthOfPassWord);
+  copyBox.value = finalgeneratedPassword;
+}
+
+// copy password function
+const copyPassword = (): void => {
+  copyBox.select();
+  copyBox.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyBox.value);
+  console.log(copyBox.value);
+};
+
+// our eventlinsters
+generatePassBtn.addEventListener("click", logicLoopFunction);
+copyPassBtn.addEventListener("click", copyPassword);
